71f1803677860e4b02fcf0672b508880a6f5042f
myltd66cntt <my.ltd.66cntt@ntu.edu.vn>
2026-03-04 15:21:37 +0700

    đã xong bài in hình ảnh cơ bản

--- bai1_bt2_inhinhanhdongian/src/bai1_bt2_inhinhanhdongian/bait2.java
+++ bai1_bt2_inhinhanhdongian/src/bai1_bt2_inhinhanhdongian/bait2.java
@@ -3,8 +3,10 @@ package bai1_bt2_inhinhanhdongian;
 public class bait2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+    	System.out.println("   *   ");
+        System.out.println("  ***  ");
+        System.out.println(" ***** ");
+        System.out.println("*******");
 	}
 
 }
