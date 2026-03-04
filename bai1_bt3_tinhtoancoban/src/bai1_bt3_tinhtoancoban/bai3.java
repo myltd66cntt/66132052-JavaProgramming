9bb65ab650766a54b6ca8e3a887dfbe0822d0f29
myltd66cntt <my.ltd.66cntt@ntu.edu.vn>
2026-03-04 15:23:53 +0700

    đã xong bài tính toán

--- bai1_bt3_tinhtoancoban/src/bai1_bt3_tinhtoancoban/bai3.java
+++ bai1_bt3_tinhtoancoban/src/bai1_bt3_tinhtoancoban/bai3.java
@@ -3,8 +3,17 @@ package bai1_bt3_tinhtoancoban;
 public class bai3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a = 9;
+		int b = 3;
 
+		System.out.println("=== CÁC PHÉP TOÁN CƠ BẢN ===");
+
+		System.out.printf("Cộng: %d + %d = %d\n", a, b, a + b);
+		System.out.printf("Trừ: %d - %d = %d\n", a, b, a - b);
+		System.out.printf("Nhân: %d * %d = %d\n", a, b, a * b);
+		System.out.printf("Chia: %d / %d = %d\n", a, b, a / b);
+
+		
 	}
 
 }
