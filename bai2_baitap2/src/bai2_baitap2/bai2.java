8a0aa537100d54e4e0171a69324fcc20294ea00c
myltd66cntt <my.ltd.66cntt@ntu.edu.vn>
2026-03-04 15:32:43 +0700

    đã xong bài chuyển đổi đổi nhiệt độ

--- bai2_baitap2/src/bai2_baitap2/bai2.java
+++ bai2_baitap2/src/bai2_baitap2/bai2.java
@@ -2,4 +2,10 @@ package bai2_baitap2;
 
 public class bai2 {
 
+	public static void main(String[] args) {
+		//F = (C * 9/5) + 32
+		double c = 25.0;
+		double f = (c * 9 / 5) + 32;
+		System.out.println(c + " độ C = " + f + " độ F");
+	}
 }
