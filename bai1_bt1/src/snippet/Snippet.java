b2fa139ec3dfa09b61995dc323bc6b403482d382
myltd66cntt <my.ltd.66cntt@ntu.edu.vn>
2026-03-04 15:28:19 +0700

    sua loi

---
+++ bai1_bt1/src/snippet/Snippet.java
@@ -0,0 +1,13 @@
+package snippet;
+
+public class Snippet {
+	// var - tự động suy luận kiểu
+	var message = "Hello, World with var!";
+	var number = 42;
+	var list = java.util.List.of("Java", "Modern", "Features");
+	
+	System.out.println(message);
+	System.out.println("Number: " + number);
+	System.out.println("List: " + list);
+}
+
