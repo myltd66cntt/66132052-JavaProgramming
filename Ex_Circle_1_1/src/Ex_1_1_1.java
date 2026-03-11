27c6ceac304659f040895cbee53f575f1e731601
myltd66cntt <my.ltd.66cntt@ntu.edu.vn>
2026-03-11 18:16:01 +0700

    xong Ex_Circle_1_1

---
+++ Ex_Circle_1_1/src/Ex_1_1_1.java
@@ -0,0 +1,15 @@
+
+
+public class Ex_1_1_1 {
+
+	public static void main(String[] args) {
+		Circle c1 = new Circle();
+		System.out.println("Hinh tron 1 co ban kinh la " + c1.getRadius() + " va dien tich la " + c1.getArea());
+		
+		Circle c2 = new Circle(2.0);
+		System.out.println("Hinh tron 2 co ban kinh la " + c2.getRadius() + " va dien tich la " + c2.getArea());
+		
+
+	}
+
+}
