a28af8bb354fab17f5fe4699806b0846bd5f2b69
myltd66cntt <my.ltd.66cntt@ntu.edu.vn>
2026-03-11 15:36:49 +0700

    moi xoa bai lm lai

--- bai2_bt4/src/bai2_bt4/bt4.java
+++ bai2_bt4/src/bai2_bt4/bt4.java
@@ -1,22 +1,14 @@
-package bai2_bt4;
 
-public class bt4 {
+public class ChuongTrinhChinh {
 
 	public static void main(String[] args) {
-		int soDien = 250;
-		double tienDien = 0;
-		if (soDien <= 50)
-			tienDien = soDien * 1678;
-	    else if (soDien <= 100)
-	    	tienDien = 50 * 1678 + (soDien - 50) * 1734;
-	    else if (soDien <= 200) 
-	        tienDien = 50 * 1678 + 50 * 1734 + (soDien - 100) * 2014;
-	    else if (soDien <= 300)
-	        tienDien = 50 * 1678 + 50 * 1734 + 100 * 2014 + (soDien - 200) * 2536;
-	    else 
-	        tienDien = 50 * 1678 + 50 * 1734 + 100 * 2014 + 100 * 2536 + (soDien - 300) * 2834;
-		System.out.println("So dien: " + soDien + "kWh");
-		System.out.println("Tien dien: " + tienDien + " dong");
-	}
+	// Tạo mới 1 sinh viên
+	SinhVien sv1 = new SinhVien();
+	// cấp/gán dữ liệu cụ thể
+	sv1. mssV="66112233";
+	sv1 .ten="Phạm Thị Tươi Tắn Lắm"; svi.tuo1=18;
+	sv1. hocBai("Lập trình Java");
+	
+}
 
 }
