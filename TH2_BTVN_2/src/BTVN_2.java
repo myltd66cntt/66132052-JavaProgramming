d378ba2e5d9fe095f2c79c01ae272d366a6d2535
myltd66cntt <my.ltd.66cntt@ntu.edu.vn>
2026-03-11 23:59:16 +0700

    xong bài

---
+++ TH2_BTVN_2/src/BTVN_2.java
@@ -0,0 +1,15 @@
+
+
+public class BTVN_2 {
+
+	public static void main(String[] args) {
+		int usd = 20;
+		int eur = 30;
+		int vnd;
+		vnd = usd * 23500;
+		System.out.println(usd + " USD = " + vnd + " VND");
+		vnd = eur * 27000;
+		System.out.println(eur + " EUR = " + vnd + " VND");
+	}
+
+}
