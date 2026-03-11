b892832c47ab0155366ab84920c85ace58e69426
myltd66cntt <my.ltd.66cntt@ntu.edu.vn>
2026-03-11 23:57:21 +0700

    btvn

---
+++ TH2_BTVN_1/src/BTVN_1.java
@@ -0,0 +1,16 @@
+
+public class BTVN_1 {
+
+	public static void main(String[] args) {
+		int gio = 45;
+		int luong = 30000;
+		double tong;
+		if (gio <= 40)
+			tong = (double) luong * gio;
+		else
+			tong = (double) luong * 40 + luong * (gio - 40) * 1.5;
+		System.out.println("Luong: " + tong);
+
+	}
+
+}
