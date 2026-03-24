81f01b1e3eaf06fd3dbdbaa1613bd7595936218e
myltd66cntt <my.ltd.66cntt@ntu.edu.vn>
2026-03-24 21:24:21 +0700

    xong bài

---
+++ Baitap/src/ltdm/Main.java
@@ -0,0 +1,27 @@
+package ltdm;
+
+public class Main {
+
+	public static void main(String[] args) {
+		HinhHoc hh1 = new HinhHoc("Hinh Vuong", "Vang");
+		hh1.hienThiThongTin();
+		
+		HinhChuNhat hcn1 = new HinhChuNhat("Hinh Chu Nhat", "Den", 5, 3);
+		System.out.print("\n");
+		hcn1.hienThiThongTin();
+		
+		HinhTron ht1 = new HinhTron("Hinh Tron", "Do", 4);
+		System.out.print("\n");
+		ht1.hienThiThongTin();
+		
+		HinhTamGiac htg1 = new HinhTamGiac("Hinh Tam Giac", "Hong", 3, 4, 5);
+		System.out.print("\n");
+		if (htg1.kiemTraHopLe()) {
+			System.out.println("Tam giac hop le");
+			htg1.hienThiThongTin();
+		}
+		else
+			System.out.println("Tam giac khong hop le");
+	}
+
+}
